5c2347190c14621e7278d3afc870c5ec8e26f6ae
Novel Corpse <romandafe94@gmail.com>
2018-12-16 20:10:44 +0300

    refs #3; added check output device for Win; created test file;

--- cgo-tty.go
+++ cgo-tty.go
@@ -1,12 +1,24 @@
 package tty
 
 /*
-#include <stdio.h>
-#include <unistd.h>
+#ifdef __linux__
+	#include <stdio.h>
+	#include <unistd.h>
 
-int IsTTY() {
-    return isatty(fileno(stdout)) ? 1 : 0;
-}
+	int IsTTY() {
+		printf("linux tty \n");
+    	return isatty(fileno(stdout)) ? 1 : 0;
+    }
+#elif _WIN32
+    #include <stdio.h>
+	#include <Windows.h>
+
+	CONSOLE_SCREEN_BUFFER_INFO sbi;
+	int IsTTY() {
+    	return GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbi) ? 1 : 0;
+	}
+#else
+#endif
 */
 import "C"
 
---
+++ cgo-tty_test.go
@@ -0,0 +1,10 @@
+package tty
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsTTY(t *testing.T) {
+	fmt.Printf("Is this device a tty? - %t\n", IsTTY())
+}
