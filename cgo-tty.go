8d68fe40fe1f9865a184323225255899e597b2f2
Novel Corpse <romandafe94@gmail.com>
2018-12-17 21:43:36 +0300

    refs #3; trying solve issue about identify tty in WSL;

--- cgo-tty.go
+++ cgo-tty.go
@@ -2,14 +2,30 @@ package tty
 
 /*
 #include <stdio.h>
-#if defined __APPLE__ || defined __linux__
+#include <sys/termios.h>
+#if defined __APPLE__ || defined __linux__ || defined __unix__
 #include <unistd.h>
-	int IsTTY() { return isatty(fileno(stdout)) ? 1 : 0; }
+	int IsTTY() {printf("Linux\n"); return isatty(fileno(stdout)) ? 1 : 0; }
 #elif __WIN32 || _WIN64
-#include <stdio.h>
 #include <Windows.h>
+	int isatty(int fd) {
+		struct termios ts;
+		return(tcgetattr(fd, &ts) != -1)
+	}
 	CONSOLE_SCREEN_BUFFER_INFO sbi;
-	int IsTTY() { return GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbi) ? 1 : 0; }
+	int IsTTY() {
+		printf("windows\n");
+		if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbi)) {
+			printf("console\n");
+			return 1;
+		} else if (isatty(1)) {
+			printf("tty\n");
+			return 1;
+		} else {
+			printf("non\n");
+			return 0;
+		}
+	}
 #else
 #endif
 */
