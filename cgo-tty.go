abccf4f049d307742766e083a02071f0f0292edb
Novel Corpse <romandafe94@gmail.com>
2018-12-22 12:37:03 +0300

    refs #3; back to ternary conditional; removed debug printfs;

--- cgo-tty.go
+++ cgo-tty.go
@@ -8,17 +8,11 @@ package tty
 #elif __WIN32 || _WIN64
 #include <Windows.h>
 	CONSOLE_SCREEN_BUFFER_INFO sbi;
-	int IsTTY() {
-		if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbi)) {
-			printf("console\n");
-			return 1;
-		}
-	}
+	int IsTTY() { return GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbi) ? 1 : 0; }
 #else
 #endif
 */
 import "C"
 
 func itob(v C.int) bool { return v != 0 }
-
-func IsTTY() bool { return itob(C.IsTTY()) }
+func IsTTY() bool       { return itob(C.IsTTY()) }
