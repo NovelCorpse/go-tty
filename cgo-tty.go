7bb72748c412c7effc494488cf5288b92ca2b05e
Novel Corpse <romandafe94@gmail.com>
2018-12-27 22:22:47 +0300

    refs #1; added winsize for Windows console;

--- cgo-tty.go
+++ cgo-tty.go
@@ -9,6 +9,7 @@ package tty
 #include <Windows.h>
 	CONSOLE_SCREEN_BUFFER_INFO sbi;
 	int IsTTY() { return GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbi) ? 1 : 0; }
+	void WinSize() {int ok = GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbi); if (ok) printf("Width: %d\n Height: %d\n", sbi.dwSize.X, sbi.dwSize.Y); }
 #else
 #endif
 */
@@ -16,3 +17,4 @@ import "C"
 
 func itob(v C.int) bool { return v != 0 }
 func IsTTY() bool       { return itob(C.IsTTY()) }
+func TermSize()         { C.WinSize() }
--- cgo-tty_test.go
+++ cgo-tty_test.go
@@ -7,4 +7,6 @@ import (
 
 func TestIsTTY(t *testing.T) {
 	fmt.Printf("Is this device a tty? - %t\n", IsTTY())
+	fmt.Printf("TTY Size: \n")
+	TermSize()
 }
